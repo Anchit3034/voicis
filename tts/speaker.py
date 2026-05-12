e8dc48f774d4feeaeefce73e4edb8032f00530b4
Anchit3034 <anchit03034@gmail.com>
2026-05-12 09:37:43 +0530

    tts modified, interrupt add in module, will add it in runtime soon

--- tts/speaker.py
+++ tts/speaker.py
@@ -1,9 +1,67 @@
-import pyttsx3
+import subprocess
+import tempfile
+import os
 
-engine=pyttsx3.init()
+PIPER_PATH = (
+    "./piper/piper"
+)
 
-engine.setProperty("rate",175)
+MODEL_PATH = (
+    "./piper/"
+    "en_US-lessac-medium.onnx"
+)
 
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
+current_process = None
+
+def stop_tts():
+
+    global current_process
+
+    if current_process:
+
+        current_process.kill()
+
+        current_process = None
+
+def speak_stream(text):
+
+    global current_process
+
+    with tempfile.NamedTemporaryFile(
+        suffix=".wav",
+        delete=False
+    ) as tmp:
+
+        wav_path = tmp.name
+
+    piper_cmd = [
+        PIPER_PATH,
+        "--model",
+        MODEL_PATH,
+        "--output_file",
+        wav_path
+    ]
+
+    piper = subprocess.Popen(
+        piper_cmd,
+        stdin=subprocess.PIPE
+    )
+
+    piper.stdin.write(
+        text.encode()
+    )
+
+    piper.stdin.close()
+
+    piper.wait()
+
+    current_process = subprocess.Popen(
+        [
+            "aplay",
+            wav_path
+        ]
+    )
+
+    current_process.wait()
+
+    os.remove(wav_path)
