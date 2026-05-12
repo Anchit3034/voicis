c024c3f30e2a05022e98ebcd0638e358b2f13c2d
Anchit3034 <anchit03034@gmail.com>
2026-05-12 15:42:41 +0530

    Made according to new architechure

--- main.py
+++ main.py
@@ -1,1 +1,65 @@
-from runtime.runtime import *
+import threading
+
+from runtime.controller import (
+    controller
+)
+
+from runtime.queues import (
+    event_queue
+)
+
+from workers.audio_worker import (
+    audio_loop
+)
+
+from workers.stt_worker import (
+    stt_loop
+)
+
+from workers.llm_worker import (
+    llm_loop
+)
+
+from workers.tts_worker import (
+    tts_loop
+)
+
+# =========================
+# THREADS
+# =========================
+
+threading.Thread(
+    target=audio_loop,
+    daemon=True
+).start()
+
+threading.Thread(
+    target=stt_loop,
+    daemon=True
+).start()
+
+threading.Thread(
+    target=llm_loop,
+    daemon=True
+).start()
+
+threading.Thread(
+    target=tts_loop,
+    daemon=True
+).start()
+
+print(
+    "=== REAL RUNTIME STARTED ==="
+)
+
+# =========================
+# EVENT LOOP
+# =========================
+
+while True:
+
+    event = event_queue.get()
+
+    controller.handle_event(
+        event
+    )
